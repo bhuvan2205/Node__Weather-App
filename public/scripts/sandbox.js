be5345184ba9c1b92410f1008338ebbd5fa75b08
EDYNAMIC\BhuvanS <bhuvan.s@altudo.co>
2022-06-20 16:26:32 +0530

    Remove unnecessory code

--- public/scripts/sandbox.js
+++ public/scripts/sandbox.js
@@ -1,16 +1,3 @@
-console.log("Client side javascript is running...");
-
-// fetch('http://puzzle.mead.io/puzzle').then((response)=> {
-
-//     response.json().then(data => {
-
-//         console.log(data);
-//     })
-// })
-
-
-
-
 
 document.querySelector('form').addEventListener('submit', (e)=> {
 
